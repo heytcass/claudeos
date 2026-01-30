4d719ca4a0a50a2b8754ee6832a63775952c2f0a
Tom Cassady <heytcass@gmail.com>
2026-01-29 22:32:46 -0500

    fix(flake): include 'self' in outputs for proper configuration

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, nixos-hardware, sops-nix }@inputs:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, sops-nix }@inputs:
     let
       lib = import ./lib { inherit (nixpkgs) lib; };
       specialArgs = { inherit inputs; };
